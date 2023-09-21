85b7e694d1cdc6d2fb52885e187fd483f8f3a145
Helcio Caixeta <helcio.caixeta@gmail.com>
2023-09-21 18:13:04 -0300

    Versão para desenvovimento

--- app.py
+++ app.py
@@ -5,8 +5,33 @@ from flask import Flask, jsonify
 from routes import main
 app = Flask(__name__)
 
-def server_app():
-    app.register_blueprint(main)
-    print('Rodando')
-       
-    return app
+class MeuProduto:
+    def __init__(self,name, price):
+        self.name = name,
+        self.price = price
+
+#producao
+#def server_app():
+#    app.register_blueprint(main)
+#    print('Rodando')
+#       
+#    return app
+
+#desenvolvimento
+from bd import products
+import json
+@app.route('/products', methods=['GET'])
+def productsList():
+    #return jsonify([
+    #    {'id':1,'name':'silva'},
+    #    {'id':3,'name':'yan'},
+    #])
+    dados = products()
+    minhalista = []
+    #for x in dados:
+    #   minhalista.append(MeuProduto(x[1],x[2]))
+          
+    return jsonify(dados)
+    
+app.run()
+
