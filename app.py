6d670501b58a45bc4fc6d922270ecc169738714c
Helcio Caixeta <helcio.caixeta@gmail.com>
2023-10-02 21:51:32 -0300

    Correcao run producao 1

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-
-import os
-from flask import Flask, request #producao: jsonify, make_response, 
+from flask import Flask 
 
 #https://www.youtube.com/watch?v=qg3BNCa_NdQ
 
 from routes import main
 app = Flask(__name__)
-
-class MeuProduto:
-    def __init__(self,name, price):
-        self.name = name,
-        self.price = price
 
 #producao
 def server_app():
